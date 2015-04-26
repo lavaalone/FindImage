adb3c562fb4622e3c523a4e5315a719647158e00
lavaalone <lavaalone@gmail.com>
2015-04-26 12:38:28 +0700

    Create project
    First commit

---
+++ src/main/java/findimage/App.java
@@ -0,0 +1,13 @@
+package findimage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
